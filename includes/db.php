3b5b90505f88e578fee2f3455a8b18fce2afe965
dreadlord-sedai <dahamifabbio@gmail.com>
2025-04-05 16:01:39 +0530

    Db.php

--- includes/db.php
+++ includes/db.php
@@ -1,16 +1,18 @@
 <?php
 
-$db['host'] = 'localhost';
-$db['user'] = 'root';
-$db['pass'] = 'mysql2006';
-$db['name'] = 'cms';
+$db['db_host'] = 'localhost';
+$db['db_user'] = 'root';
+$db['db_pass'] = 'mysql2006';
+$db['db_name'] = 'cms';
+
 
 // Define constants for database connection
 foreach($db as $key => $value){
     define(strtoupper($key), $value);
+   
 }
 
-$connection = mysqli_connect("localhost", "root", "mysql2006", "cms");
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS,DB_NAME);
 
 if($connection) {
     echo "Connection successful";
