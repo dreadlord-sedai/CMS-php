6362d3c9f25f640c7a2925fc5c89bab1195c3977
dreadlord-sedai <dahamifabbio@gmail.com>
2025-04-05 17:28:54 +0530

    Add Dynamic posts

--- index.php
+++ index.php
@@ -21,6 +21,50 @@ include "includes/header.php";
                 </h1>
 
                 <!-- First Blog Post -->
+
+                <?php
+                $query = "SELECT * FROM posts";
+                $select_all_posts_query = mysqli_query($connection, $query);
+
+                while($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = substr($row['post_content'], 0, 100);
+                    $post_status = $row['post_status'];
+                    $post_id = $row['post_id'];
+                    $post_comment_count = $row['post_comment_count'];
+                    $post_category_id = $row['post_category_id'];
+                    $post_tags = $row['post_tags'];
+
+                    ?>
+
+                    <h2>
+                        <a href="#"><?php echo $post_title; ?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="#"><?php echo $post_author; ?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
+                    <hr>
+                    <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                    <hr>
+                    <p><?php echo $post_content; ?></p>
+                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                    <hr>
+
+
+
+                    <?php
+                }
+                    ?>
+
+                    
+
+
+
                 <h2>
                     <a href="#">Blog Post Title</a>
                 </h2>
