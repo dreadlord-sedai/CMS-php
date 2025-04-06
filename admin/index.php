5c69e137c045e4c29a2a8a9c28632d5e3cd41bde
dreadlord-sedai <dahamifabbio@gmail.com>
2025-04-06 10:59:11 +0530

    Admin nav

--- admin/index.php
+++ admin/index.php
@@ -2,34 +2,42 @@
 include "includes/header.php";
 ?>
 
-    <div id="wrapper">
+<div id="wrapper">
 
-        <!-- Navigation -->
-        <?php
-        include "includes/navigation.php";
-        ?>
 
-        <div id="page-wrapper">
-            <div class="container-fluid">
+    <!-- Navigation -->
+    <?php
+    include "includes/navigation.php";
+    ?>
 
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Welcome to Admin
-                            <small>Author</small>
-                        </h1>
+    <div id="page-wrapper">
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Welcome to Admin
+                        <small>Author</small>
+                    </h1>
+                    <div class="col-xs-6">
+                        <form action="">
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary" name="submit" value="Add Post">
+                            </div>
+                        </form>
                     </div>
                 </div>
-                <!-- /.row -->
-
             </div>
-            <!-- /.container-fluid -->
+            <!-- /.row -->
 
         </div>
-        <!-- /#page-wrapper -->
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
 
     <?php
     include "includes/footer.php";
-    
+
     ?>
