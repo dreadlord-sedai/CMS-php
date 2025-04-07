81e7372bfce48b128d19760ca5e272e3b90be870
dreadlord-sedai <dahamifabbio@gmail.com>
2025-04-07 15:37:37 +0530

    Refactor Posts

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,3 +1,22 @@
+<?php
+if (isset($_POST['title'])) {
+    $post_title = $_POST['title'];
+    $post_author = $_POST['author'];
+    $post_category_id = $_POST['post_category_id'];
+    $post_status = $_POST['post_status'];
+
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+
+    $post_tags = $_POST['post_tags'];
+    $post_content = $_POST['post_content'];
+    $post_date = date('d-m-y');
+    $post_comment_count = 4;
+    
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+}
+?>
+
 <form action="" method="post" enctype="multipart/form-data">
 
     <div class="form-group">
@@ -9,7 +28,7 @@
         <label for="post_category">Post Category Id</label>
         <input type="text" class="form-control" name="post_category_id">
     </div>
-    
+
     <div class="form-group">
         <label for="title">Post Author</label>
         <input type="text" class="form-control" name="author">
@@ -36,7 +55,7 @@
     </div>
 
     <div class="form-group">
-        <input type="button" value="Publish Post" name="submit" class="btn btn-primary">
+        <input type="submit" value="Publish Post" name="submit" class="btn btn-primary">
     </div>
 
 </form>
