069d49cadfda7aa5723117624739e604e8996aa1
dreadlord-sedai <dahamifabbio@gmail.com>
2025-04-07 10:04:03 +0530

    Posts page

--- admin/posts.php
+++ admin/posts.php
@@ -37,35 +37,26 @@ include "includes/admin_header.php";
                     </thead>
                     <tbody>
 
-                        <!-- Display Posts from Database -->
-                        <?php
+                        <!-- Display posts -->
+                        <?php   
+                         // 
+                         if (isset($_GET['source'])) {
+                             $source = $_GET['source'];
+                         }else {
+                             $source = '';
+                         }
 
-                        $query = "SELECT * FROM posts";
-                        $select_posts = mysqli_query($connection, $query);
-
-                        while ($row = mysqli_fetch_assoc($select_posts)) {
-                            $post_id = $row['post_id'];
-                            $post_author = $row['post_author'];
-                            $post_title = $row['post_title'];
-                            $post_category = $row['post_category_id'];
-                            $post_status = $row['post_status'];
-                            $post_image = $row['post_image'];
-                            $post_tags = $row['post_tags'];
-                            $post_comment_count = $row['post_comment_count'];
-                            $post_date = $row['post_date'];
-
-                            echo "<tr>";
-                            echo "<td>{$post_id}</td>";
-                            echo "<td>{$post_author}</td>";
-                            echo "<td>{$post_title}</td>";
-                            echo "<td>{$post_category}</td>";
-                            echo "<td>{$post_status}</td>";
-                            echo "<td><img class='img-responsive' width='100' src='../images/{$post_image}' alt='image'></td>";
-                            echo "<td>{$post_tags}</td>";
-                            echo "<td>{$post_comment_count}</td>";
-                            echo "<td>{$post_date}</td>";
-                            echo "</tr>";
-                        }
+                         switch ($source) {
+                             case 'add_post':
+                                 include "includes/add_post.php";
+                                 break;
+                             case 'edit_post':
+                                 include "includes/edit_post.php";
+                                 break;
+                             default:
+                                 include "includes/view_all_posts.php";
+                                 break;
+                         }
                         ?>
                     </tbody>
                 </table>
