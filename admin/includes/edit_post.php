f6490c6edf37b0dca0311b708e34b9555dd14e39
dreadlord-sedai <dahamifabbio@gmail.com>
2025-04-07 18:00:46 +0530

    edit post

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -5,7 +5,7 @@ if (isset($_GET['p_id'])) {
 } else {
     $the_post_id = '';
 }
-
+// Update Post
 $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
 $select_posts_by_id = mysqli_query($connection, $query);
 
@@ -13,16 +13,46 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     $post_id = $row['post_id'];
     $post_author = $row['post_author'];
     $post_title = $row['post_title'];
-    $post_category = $row['post_category_id'];
     $post_status = $row['post_status'];
     $post_image = $row['post_image'];
     $post_tags = $row['post_tags'];
-    $post_comment_count = $row['post_comment_count'];
-    $post_date = $row['post_date'];
     $post_content = $row['post_content'];
 }
-?>
 
+if (isset($_POST['update_post'])) {
+    $post_title = $_POST['title'];
+    $post_author = $_POST['author'];
+    $post_category_id = $_POST['post_category'];
+    $post_status = $_POST['post_status'];
+
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+    $post_tags = $_POST['post_tags'];
+    $post_content = $_POST['post_content'];
+
+    $post_date = date('d-m-y');
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    $query = "UPDATE posts SET ";    
+    $query .= "post_title = '{$post_title}', ";
+    $query .= "post_category_id = '{$post_category_id}', ";
+    $query .= "post_date = now(), ";
+    $query .= "post_author = '{$post_author}', ";
+    $query .= "post_status = '{$post_status}', ";
+    $query .= "post_tags = '{$post_tags}', ";
+    $query .= "post_content = '{$post_content}', ";
+    $query .= "post_image = '{$post_image}' ";
+    $query .= "WHERE post_id = {$the_post_id} ";
+    
+
+    $update_post = mysqli_query($connection, $query);
+
+    confirm_query($update_post);
+    header("Location: posts.php");
+} 
+
+
+?>
 <form action="" method="post" enctype="multipart/form-data">
 
     <div class="form-group">
@@ -36,6 +66,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
         <label for="post_category">Post Category Id</label>
         <select name="post_category" id="post_category" class="form-control">
             <?php
+            // Fetch categories from the database
             $query = "SELECT * FROM categories";
             $select_categories = mysqli_query($connection, $query);
 
@@ -85,7 +116,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     </div>
 
     <div class="form-group">
-        <input type="submit" value="Publish Post" name="submit" class="btn btn-primary">
+        <input type="submit" value="Update Post" name="update_post" class="btn btn-primary">
     </div>
 
 </form>
