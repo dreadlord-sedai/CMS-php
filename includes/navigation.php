fac041730d7e8496d207b04af89abb66f6b85a39
dreadlord-sedai <dahamifabbio@gmail.com>
2025-04-09 19:01:20 +0530

    Create category page

--- includes/navigation.php
+++ includes/navigation.php
@@ -8,10 +8,10 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Start Bootstrap</a>
+                <a class="navbar-brand" href="index.php">Start Bootstrap</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div class="collapse navbar-collapse" id="bs-e xample-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <?php
                     include "includes/db.php";
@@ -19,9 +19,10 @@
                     $query = "SELECT * FROM categories";
                     $select_all_categories_query = mysqli_query($connection, $query);
 
-                    while($row = mysqli_fetch_assoc($select_all_categories_query)) {
+                    while ($row = mysqli_fetch_assoc($select_all_categories_query)) {
                         $cat_title = $row['cat_title'];
-                        echo "<li><a href='#'>{$cat_title}</a></li>";
+                        $cat_id = $row['id'];
+                        echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
                     }
                     ?>
                     <li>
