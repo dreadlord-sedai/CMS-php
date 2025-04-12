fd63dfcc7471e88e71e1b8a0b9b054242e2afeb2
dreadlord-sedai <dahamifabbio@gmail.com>
2025-04-12 11:55:57 +0530

    Users

---
+++ admin/includes/add_users.php
@@ -0,0 +1,83 @@
+<?php
+if (isset($_POST['title'])) {
+    $post_title = $_POST['title'];
+    $post_author = $_POST['author'];
+    $post_category_id = $_POST['post_category_id'];
+    $post_status = $_POST['post_status'];
+
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+
+    $post_tags = $_POST['post_tags'];
+    $post_content = $_POST['post_content'];
+    $post_date = date('d-m-y');
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    $query = "INSERT INTO posts(post_title, post_category_id, post_date, post_author, post_image, post_content, post_tags, post_status, post_comment_count)
+              VALUES('{$post_title}', '{$post_category_id}', now(), '{$post_author}', '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_status}', '0')";
+
+    $create_post_query = mysqli_query($connection, $query);
+
+    confirm_query($create_post_query);
+
+    header("Location: posts.php?source=add_post");
+}
+?>
+
+<form action="" method="post" enctype="multipart/form-data">
+
+    <div class="form-group">
+        <label for="title">Post Title</label>
+        <input type="text" class="form-control" name="title">
+    </div>
+
+    <div class="form-group">
+        <label for="post_category">Post Category</label>
+        <select name="post_category_id" id="post_category_id" class="form-control">
+            <?php
+            // Fetch categories from the database
+            $query = "SELECT * FROM categories";
+            $select_categories = mysqli_query($connection, $query);
+
+            confirm_query($select_categories);
+
+            while ($row = mysqli_fetch_assoc($select_categories)) {
+                $cat_title = $row['cat_title'];
+                $cat_id = $row['id'];
+                echo "<option value='{$cat_id}'>{$cat_title}</option>";
+            }
+            ?>
+        </select>
+    </div>
+
+    <div class="form-group">
+        <label for="title">Post Author</label>
+        <input type="text" class="form-control" name="author">
+    </div>
+
+    <div class="form-group">
+        <label for="post_status">Post Status</label>
+        <input type="text" class="form-control" name="post_status">
+    </div>
+
+    <div class="form-group">
+        <label for="post_image">Post Image</label>
+        <input type="file" name="image">
+    </div>
+
+    <div class="form-group">
+        <label for="post_tags">Post Tags</label>
+        <input type="text" class="form-control" name="post_tags">
+    </div>
+
+    <div class="form-group">
+        <label for="post_content">Post Content</label>
+        <textarea class="form-control" name="post_content" id="" cols="30" rows="10"></textarea>
+    </div>
+
+    <div class="form-group">
+        <input type="submit" value="Publish Post" name="submit" class="btn btn-primary">
+    </div>
+
+</form>
