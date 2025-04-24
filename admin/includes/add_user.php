afdb254d1e8badcbe7f6a5c889f5b24df8d33f59
dreadlord-sedai <dahamifabbio@gmail.com>
2025-04-24 16:06:42 +0530

    Users7

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -1,45 +1,46 @@
 <?php
 if (isset($_POST['create_user'])) {
-    $user_id = $_POST['user_id'];
-    $post_author = $_POST['author'];
-    $post_category_id = $_POST['post_category_id'];
-    $post_status = $_POST['post_status'];
+    echo $user_firstname = $_POST['user_firstname'];
+    $user_lastname = $_POST['user_lastname'];
+    $user_role = $_POST['user_role'];
 
-    $post_image = $_FILES['image']['name'];
-    $post_image_temp = $_FILES['image']['tmp_name'];
+    // $post_image = $_FILES['image']['name'];
+    // $post_image_temp = $_FILES['image']['tmp_name'];
 
-    $post_tags = $_POST['post_tags'];
-    $post_content = $_POST['post_content'];
-    $post_date = date('d-m-y');
+    $username = $_POST['username'];
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
+    // $post_date = date('d-m-y');
 
-    move_uploaded_file($post_image_temp, "../images/$post_image");
+    // move_uploaded_file($post_image_temp, "../images/$post_image");
 
-    $query = "INSERT INTO users(post_title, post_category_id, post_date, post_author, post_image, post_content, post_tags, post_status, post_comment_count)
-              VALUES('{$post_title}', '{$post_category_id}', now(), '{$post_author}', '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_status}', '0')";
+    // $query = "INSERT INTO users(post_title, post_category_id, post_date, post_author, post_image, post_content, post_tags, post_status, post_comment_count)
+    //           VALUES('{$post_title}', '{$post_category_id}', now(), '{$post_author}', '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_status}', '0')";
 
-    $create_post_query = mysqli_query($connection, $query);
+    // $create_post_query = mysqli_query($connection, $query);
 
-    confirm_query($create_post_query);
+    // confirm_query($create_post_query);
 
-    header("Location: posts.php?source=add_post");
+    // header("Location: posts.php?source=add_post");
 }
 ?>
 
 <form action="" method="post" enctype="multipart/form-data">
 
     <div class="form-group">
-        <label for="title">Firstname</label>
-        <input type="text" class="form-control" name="title">
+        <label for="user_firstname">Firstname</label>
+        <input type="text" class="form-control" name="user_firstname">
     </div>
 
     <div class="form-group">
-        <label for="post_status">Lastname</label>
-        <input type="text" class="form-control" name="post_status">
+        <label for="user_lastname">Lastname</label>
+        <input type="text" class="form-control" name="user_lastname">
     </div>
 
     <div class="form-group">
         <label for="user_role">Role</label>
         <select name="user_role" id="">
+            <option value="">Select Option</option>
             <option value="admin">Admin</option>
             <option value="subscriber">Subscriber</option>
 
@@ -59,8 +60,8 @@ if (isset($_POST['create_user'])) {
 
 
     <div class="form-group">
-        <label for="title">Username</label>
-        <input type="text" class="form-control" name="author">
+        <label for="username">Username</label>
+        <input type="text" class="form-control" name="username">
     </div>
 
 
@@ -70,19 +71,19 @@ if (isset($_POST['create_user'])) {
     </div> -->
 
     <div class="form-group">
-        <label for="post_tags">Email</label>
-        <input type="text" class="form-control" name="post_tags">
+        <label for="user_email">Email</label>
+        <input type="text" class="form-control" name="user_email">
     </div>
 
     <div class="form-group">
-        <label for="post_status">Password</label>
-        <input type="text" class="form-control" name="post_status">
+        <label for="user_passsword">Password</label>
+        <input type="text" class="form-control" name="user_password">
     </div>
 
 
 
     <div class="form-group">
-        <input type="submit" value="Publish Post" name="submit" class="btn btn-primary">
+        <input type="submit" value="create_user" name="create_user" class="btn btn-primary">
     </div>
 
 </form>
