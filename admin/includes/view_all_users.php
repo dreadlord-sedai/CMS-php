0639786cc9d4f768bd05e2cc84422f9c05911795
dreadlord-sedai <dahamifabbio@gmail.com>
2025-04-26 17:48:40 +0530

    Users12

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -36,7 +36,7 @@
         echo "<td>{$user_lastname}</td>";
         echo "<td>{$user_email}</td>";
         echo "<td>{$user_role}</td>";
-        echo "<td><a href='users.php?source=edit_user={$user_id}'>Edit</a></td>";
+        echo "<td><a href='users.php?source=edit_user&edit_user={$user_id}'>Edit</a></td>";
         echo "<td><a href='users.php?change_to_admin=$user_id'>Admin</a></td>";
         echo "<td><a href='users.php?change_to_subscriber=$user_id'>Subscriber</a></td>";
         echo "<td><a href='users.php?delete=$user_id'>Delete</a></td>";
