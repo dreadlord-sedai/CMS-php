bd54c56856c8d103ebd90ab95f8f50cde50000b4
dreadlord-sedai <dahamifabbio@gmail.com>
2025-04-29 11:58:37 +0530

    Profile

--- admin/profile.php
+++ admin/profile.php
@@ -22,26 +22,6 @@ include "includes/admin_header.php";
                 </div>
 
                 <!-- Display Source -->
-                <?php
-                // 
-                if (isset($_GET['source'])) {
-                    $source = $_GET['source'];
-                } else {
-                    $source = '';
-                }
-
-                switch ($source) {
-                    case 'add_user':
-                        include "includes/add_user.php";
-                        break;
-                    case 'edit_user':
-                        include "includes/edit_user.php";
-                        break;
-                    default:
-                        include "includes/view_all_users.php";
-                        break;
-                }
-                ?>
 
 
             </div>
