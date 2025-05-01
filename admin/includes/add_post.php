3f97506d5e4a836411a632a4fce0a0786e166ef8
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-01 11:05:14 +0530

    Addition features: Summernote

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -73,7 +73,7 @@ if (isset($_POST['title'])) {
 
     <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea class="form-control" name="post_content" id="" cols="30" rows="10"></textarea>
+        <textarea class="form-control" name="post_content" id="summernote" cols="30" rows="10"></textarea>
     </div>
 
     <div class="form-group">
