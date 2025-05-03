23302082956fb38507325666ac90066a8b574c91
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-03 12:16:42 +0530

    Addition features10

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -4,9 +4,7 @@
 if (isset($_POST['checkBoxArray'])) {
     foreach ($_POST['checkBoxArray'] as $postValueId) {
         $bulk_options = $_POST['bulk_options'];
-
-        
-        }
+    }
     header("Location: posts.php");
 }
 
@@ -15,13 +13,19 @@ if (isset($_POST['checkBoxArray'])) {
 <form action="" method="post">
 
     <table class="table table-bordered table-hover">
-        <div id="bulkOptionsContainer" class="col-xs-4" style="padding: 0px;">
-            <select class="form-control" name="" id="">
+        <div id="bulkOptionsContainer" class="col-xs-4">
+            <select class="form-control" name="bulk_options" id="">
                 <option value="">Select Options</option>
                 <option value="published">Publish</option>
                 <option value="draft">Draft</option>
                 <option value="delete">Delete</option>
             </select>
+
+        </div>
+
+        <div class="col-xs-4">
+            <button type="submit" name="submit" class="btn btn-success" value="Apply">Apply</button>
+            <a class="btn btn-primary" href="posts.php?source=add_post">Add New</a>
         </div>
 
         <thead>
@@ -51,7 +55,7 @@ if (isset($_POST['checkBoxArray'])) {
             $post_author = $row['post_author'];
             $post_title = $row['post_title'];
             $post_category_id = $row['post_category_id'];
-            $post_status = $row['post_status']; 
+            $post_status = $row['post_status'];
             $post_image = $row['post_image'];
             $post_tags = $row['post_tags'];
             $post_comment_count = $row['post_comment_count'];
