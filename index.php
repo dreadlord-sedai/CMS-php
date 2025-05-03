159dabaf2eec56c8139b388ee029c00ee186897d
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-03 09:12:33 +0530

    Addition features4

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ include "includes/navigation.php";
                 if ($post_status !== 'published') {
                     continue;
                 }
-                
+
 
             ?>
 
@@ -53,7 +53,10 @@ include "includes/navigation.php";
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
                 <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                <a href="post.php?p_id=<?php echo $post_id; ?>">
+                    <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                </a>
+
                 <hr>
                 <p><?php echo $post_content; ?></p>
                 <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
