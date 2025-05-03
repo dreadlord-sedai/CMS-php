2a096b267d50d9bc5e3ac0714eeb7733e792f43d
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-03 09:17:57 +0530

    Addition features4

--- index.php
+++ index.php
@@ -59,7 +59,9 @@ include "includes/navigation.php";
 
                 <hr>
                 <p><?php echo $post_content; ?></p>
-                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                </a>
 
                 <hr>
 
