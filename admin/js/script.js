70731ea30233b351d235050465615acc4f5524bf
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-03 12:50:26 +0530

    Addition features12

--- admin/js/script.js
+++ admin/js/script.js
@@ -1,21 +1,19 @@
-$(document).ready(function() {
-    $('#selectAllBoxes').click(function(event) {
-        if (this.checked) {
-            $('.checkBoxes').each(function() {
-                this.checked = true;
-            });
-        } else {
-            $('.checkBoxes').each(function() {
-                this.checked = false;
-            });
-        }
+$(document).ready(function () {
+  $("#selectAllBoxes").click(function (event) {
+    if (this.checked) {
+      $(".checkBoxes").each(function () {
+        this.checked = true;
+      });
+    } else {
+      $(".checkBoxes").each(function () {
+        this.checked = false;
+      });
     }
-    );
+  });
 });
 
-$(document).ready(function() {
-    $('#summernote').summernote({
-        height: 300 // Set the height of the editor
-    });
+$(document).ready(function () {
+  $("#summernote").summernote({
+    height: 300, // Set the height of the editor
+  });
 });
-
