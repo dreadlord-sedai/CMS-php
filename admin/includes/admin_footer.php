fe4275fa5c3464ba1de6b42ef8c99a19274b5bb6
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-07 10:47:37 +0530

    Addition features13

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -1,14 +1,17 @@
 </div>
 <!-- /#wrapper -->
 
-<!-- Bootstrap Core JavaScript -->
-<script src="/CMS-php/admin/js/bootstrap.min.js"></script>
+
 
 <!-- Custom JavaScript -->
  <script src="js/jquery.js"></script>
+ <!-- Bootstrap Core JavaScript -->
+<script src="/CMS-php/admin/js/bootstrap.min.js"></script>
+
+<script src="./js/summernote.min.js"></script>
 <script src="js/script.js"></script>
 
-<!-- <script src="./js/summernote.min.js"></script> -->
+
 
 </body>
 
--- admin/js/script.js
+++ admin/js/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
+  
+  // Summernote WYSIWYG Editor
   $("#summernote").summernote({
     height: 300, // Set the height of the editor
   });
-});
 
-$(document).ready(function () {
+  // Bulk Action
   $("#selectAllBoxes").click(function (event) {
     if (this.checked) {
       $(".checkBoxes").each(function () {
