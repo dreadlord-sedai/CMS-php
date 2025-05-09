5e0c10cdc64d583823529117c2c6115dd332a0fe
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-09 09:10:58 +0530

    Registration4

--- registration.php
+++ registration.php
@@ -14,6 +14,14 @@
         $username = mysqli_real_escape_string($connection, $username);
         $email = mysqli_real_escape_string($connection, $email);
         $password = mysqli_real_escape_string($connection, $password);
+
+        $query = "SELECT randSalt FROM users";
+        $select_randsalt_query = mysqli_query($connection, $query);
+
+        if (!$select_randsalt_query) {
+            die("Query Failed" . mysqli_error($connection));
+        }
+        
     }
 
 
