583c5b5e1394adac5898885ecce6534ce6ff3d70
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-10 11:12:08 +0530

    Author Posts6

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -133,7 +133,7 @@ if (isset($_POST['checkBoxArray'])) {
             echo "<td>{$post_tags}</td>";
             echo "<td>{$post_comment_count}</td>";
             echo "<td>{$post_date}</td>";
-            echo "<td>{$post_views_count}</td>";
+            echo "<td><a href='../posts.php?reset={$post_id}'>{$post_views_count}</a></td>";
             echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
             echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
             echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete?');\" href='posts.php?delete={$post_id}'>Delete</a></td>";
@@ -148,8 +148,16 @@ if (isset($_POST['checkBoxArray'])) {
 // Delete Post
 if (isset($_GET['delete'])) {
     $the_post_id = $_GET['delete'];
-    $query = "DELETE FROM posts WHERE post_id = {$the_post_id}";
+    $query = "DELETE FROM posts WHERE post_id = " . mysqli_real_escape_string($connection, $the_post_id) . "";
     $delete_query = mysqli_query($connection, $query);
+    header("Location: posts.php");
+}
+
+// Reset Post Views
+if (isset($_GET['reset'])) {
+    $the_post_id = $_GET['reset'];
+    $query = "UPDATE posts SET post_views_count = 0 WHERE post_id = " . mysqli_real_escape_string($connection, $the_post_id) . "";
+    $reset_query = mysqli_query($connection, $query);
     header("Location: posts.php");
 }
 
