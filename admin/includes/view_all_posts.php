c8e858995e074aacf61df19fa8b8be8cb1938927
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-10 10:36:47 +0530

    Author Posts3

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -23,6 +23,29 @@ if (isset($_POST['checkBoxArray'])) {
                 $delete_post = mysqli_query($connection, $query);
                 confirm_query($delete_post);
                 break;
+
+            case 'clone':
+                $query = "SELECT * FROM posts WHERE post_id = {$postValueId}";
+                $select_post_query = mysqli_query($connection, $query);
+
+                while ($row = mysqli_fetch_array($select_post_query)) {
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = $row['post_content'];
+                    $post_status = $row['post_status'];
+                    $post_comment_count = $row['post_comment_count'];
+                    $post_category_id = $row['post_category_id'];
+                    $post_tags = $row['post_tags'];
+
+                }
+                $query = "INSERT INTO posts(post_title, post_author, post_date, post_image, post_content, post_status, post_comment_count, post_category_id, post_tags) ";
+                $query .= "VALUES('{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_status}', {$post_comment_count}, {$post_category_id}, '{$post_tags}')";
+                $clone_post = mysqli_query($connection, $query);
+                confirm_query($clone_post);
+
+                break;
         }
     }
     header("Location: posts.php");
@@ -39,6 +62,7 @@ if (isset($_POST['checkBoxArray'])) {
                 <option value="published">Publish</option>
                 <option value="draft">Draft</option>
                 <option value="delete">Delete</option>
+                <option value="clone">Clone</option>
             </select>
 
         </div>
