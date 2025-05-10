da8ee653917778d57c6319a3db1d48457cef7d51
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-10 10:30:18 +0530

    Author Posts2

--- author_posts.php
+++ author_posts.php
@@ -50,7 +50,7 @@ include "includes/navigation.php";
                     <a href="#"><?php echo $post_title; ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="author_posts.php?author=<?php echo $post_author; ?>&p_id=<?php echo $the_post_id; ?>"><?php echo $post_author; ?></a>
+                    All Posts by <a href="author_posts.php?author=<?php echo $post_author; ?>&p_id=<?php echo $the_post_id; ?>"><?php echo $post_author; ?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
                 <hr>
