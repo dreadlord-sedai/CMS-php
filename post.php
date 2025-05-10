c82f1abca82f36eb025854a60d7d61a082643348
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-10 10:59:08 +0530

    Author Posts5

--- post.php
+++ post.php
@@ -20,48 +20,51 @@ include "includes/navigation.php";
             // Check if 'p_id' is set in the URL
             if (isset($_GET['p_id'])) {
                 $the_post_id = $_GET['p_id'];
-            }
 
-            $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
-            $select_all_posts_query = mysqli_query($connection, $query);
 
-            while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
-                $post_title = $row['post_title'];
-                $post_author = $row['post_author'];
-                $post_date = $row['post_date'];
-                $post_image = $row['post_image'];
-                $post_content = substr($row['post_content'], 0, 100);
-                $post_status = $row['post_status'];
-                $post_comment_count = $row['post_comment_count'];
-                $post_category_id = $row['post_category_id'];
-                $post_tags = $row['post_tags'];
+                $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
+                $select_all_posts_query = mysqli_query($connection, $query);
+
+                while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = substr($row['post_content'], 0, 100);
+                    $post_status = $row['post_status'];
+                    $post_comment_count = $row['post_comment_count'];
+                    $post_category_id = $row['post_category_id'];
+                    $post_tags = $row['post_tags'];
 
             ?>
 
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
+                    <h1 class="page-header">
+                        Page Heading
+                        <small>Secondary Text</small>
+                    </h1>
 
-                <!-- First Blog Post -->
+                    <!-- First Blog Post -->
 
-                <h2>
-                    <a href="#"><?php echo $post_title; ?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="#"><?php echo $post_author; ?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
-                <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                <hr>
-                <p><?php echo $post_content; ?></p>
-            
-                <hr>
+                    <h2>
+                        <a href="#"><?php echo $post_title; ?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="#"><?php echo $post_author; ?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
+                    <hr>
+                    <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                    <hr>
+                    <p><?php echo $post_content; ?></p>
+
+                    <hr>
 
 
 
             <?php
+                }
+            } else {
+                header("Location: index.php");
             }
             ?>
 
