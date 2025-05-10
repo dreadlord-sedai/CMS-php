c867942eda289dd586b7600df7b68a7ead3cd558
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-10 10:28:33 +0530

    Author Posts2

--- author_posts.php
+++ author_posts.php
@@ -20,9 +20,10 @@ include "includes/navigation.php";
             // Check if 'p_id' is set in the URL
             if (isset($_GET['p_id'])) {
                 $the_post_id = $_GET['p_id'];
+                $the_post_author = $_GET['author'];
             }
 
-            $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
+            $query = "SELECT * FROM posts WHERE post_author = '{$the_post_author}'";
             $select_all_posts_query = mysqli_query($connection, $query);
 
             while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
@@ -56,109 +57,12 @@ include "includes/navigation.php";
                 <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
                 <hr>
                 <p><?php echo $post_content; ?></p>
-            
+
                 <hr>
 
-
-
             <?php
             }
             ?>
-
-            <!-- Blog Comments -->
-
-            <?php
-
-            if (isset($_POST['create_comment'])) {
-
-                $the_post_id = $_GET['p_id'];
-                $comment_author = $_POST['comment_author'];
-                $comment_email = $_POST['comment_email'];
-                $comment_content = $_POST['comment_content'];
-
-
-                if (!empty($comment_author) && !empty($comment_email) && !empty($comment_content)) {
-
-
-                    $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status,comment_date)";
-
-                    $query .= "VALUES ($the_post_id ,'{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved',now())";
-
-                    $create_comment_query = mysqli_query($connection, $query);
-
-                    if (!$create_comment_query) {
-                        die('QUERY FAILED' . mysqli_error($connection));
-                    }
-
-                    // Increment comment count in posts table
-                    $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 WHERE post_id = $the_post_id";
-                    $update_comment_count = mysqli_query($connection, $query);
-                    if (!$update_comment_count) {
-                        die('QUERY FAILED' . mysqli_error($connection));
-                    }
-
-                    header("Location: post.php?p_id=$the_post_id");
-                }
-            }
-            ?>
-
-            <!-- Comments Form -->
-            <div class="well">
-                <h4>Leave a Comment:</h4>
-                <form action="#" method="post" role="form">
-
-                    <div class="form-group">
-                        <label for="Author">Author</label>
-                        <input type="text" name="comment_author" class="form-control" name="comment_author">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="Author">Email</label>
-                        <input type="email" name="comment_email" class="form-control" name="comment_email">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="comment">Your Comment</label>
-                        <textarea name="comment_content" class="form-control" rows="3"></textarea>
-                    </div>
-                    <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
-                </form>
-            </div>
-
-            <hr>
-
-            <!-- Posted Comments -->
-            <?php
-            // Display comments
-            $query = "SELECT * FROM comments WHERE comment_post_id = $the_post_id AND comment_status = 'approved' ORDER BY comment_id DESC";
-            $select_comment_query = mysqli_query($connection, $query);
-            if (!$select_comment_query) {
-                die('QUERY FAILED' . mysqli_error($connection));
-            }
-            while ($row = mysqli_fetch_array($select_comment_query)) {
-                $comment_date = $row['comment_date'];
-                $comment_content = $row['comment_content'];
-                $comment_author = $row['comment_author'];
-
-            ?>
-
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading"><?php echo $comment_author; ?>
-                            <small><?php echo $comment_date; ?></small>
-                        </h4>
-                        <?php echo $comment_content; ?>
-                    </div>
-                </div>
-
-            <?php
-            }
-            ?>
-
 
 
         </div>
