679dfc5f4c68df948533052272e570226f98ffc9
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-11 13:35:00 +0530

    Pagination5

--- index.php
+++ index.php
@@ -112,7 +112,12 @@ include "includes/navigation.php";
     <ul class="pager">
         <?php
         for ($i = 1; $i <= $count; $i++) {
-            echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+            if ($i == $page) {
+                echo "<li><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
+            } else {
+                echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+            }
+            
         }
         ?>
     </ul>
