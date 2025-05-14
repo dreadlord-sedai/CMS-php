2c63ad7541d0a1bba1396f4c4f24b06e038e9072
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-14 11:00:33 +0530

    users online5

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -11,14 +11,7 @@
     </div>
     <!-- Top Menu Items -->
     <ul class="nav navbar-right top-nav">
-        <li><a href="#">Users Online:
-
-                <?php
-
-                echo users_online();
-
-                ?>
-            </a></li>
+        <li><a href="#">Users Online: <span class="usersonline"></span></a></li>
         <li><a href="../index.php">Home Site <fa class="fa fa-home"></fa></a></li>
         <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-envelope"></i> <b class="caret"></b></a>
--- admin/js/script.js
+++ admin/js/script.js
@@ -25,5 +25,5 @@ function loadUsersOnline() {
     $(".usersonline").text(data);
   });
 }
-
+loadUsersOnline();
 
