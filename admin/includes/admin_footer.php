59ae7f45d7f28280c660ca1868a5b9b4cd16321f
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-15 10:11:23 +0530

    UI Refactor

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -1,11 +1,15 @@
 </div>
 <!-- /#wrapper -->
 
-
+<footer class="admin-footer">
+    <div class="container">
+        <p>Copyright &copy; 2025 dreadlord_sedai</p>
+    </div>
+</footer>
 
 <!-- Custom JavaScript -->
- <script src="js/jquery.js"></script>
- <!-- Bootstrap Core JavaScript -->
+<script src="js/jquery.js"></script>
+<!-- Bootstrap Core JavaScript -->
 <script src="/CMS-php/admin/js/bootstrap.min.js"></script>
 
 <!-- Summernote JavaScript -->
@@ -15,6 +19,16 @@
 <!-- Custom Scripts -->
 <script src="js/script.js"></script>
 
+<style>
+.admin-footer {
+    background-color: var(--admin-card-bg);
+    color: var(--admin-text);
+    padding: 1rem 0;
+    text-align: center;
+    border-top: 1px solid var(--admin-border);
+    margin-top: 2rem;
+}
+</style>
 
 </body>
 
