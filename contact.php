1e660ba6db78420f19b5d412e71870a65f944f97
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-15 09:23:07 +0530

    Emails2

--- contact.php
+++ contact.php
@@ -6,50 +6,11 @@
 
 if (isset($_POST['submit'])) {
 
+    $to = $_POST['dahamifabbio@gmail.com'];
+    $subject = $_POST['subject'];
+    $body = $_POST['message'];
 
-
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-
-    // Validate the form
-    if (!empty($username) && !empty($email) && !empty($password)) {
-
-        $username = mysqli_real_escape_string($connection, $username);
-        $email = mysqli_real_escape_string($connection, $email);
-        $password = mysqli_real_escape_string($connection, $password);
-
-        $query = "SELECT randSalt FROM users";
-        $select_randsalt_query = mysqli_query($connection, $query);
-
-        if (!$select_randsalt_query) {
-            die("Query Failed" . mysqli_error($connection));
-        }
-
-        while ($row = mysqli_fetch_array($select_randsalt_query)) {
-            $salt = $row['randSalt'];
-        }
-        
-        // Hash the password with the salt
-        $password = crypt($password, $salt);
-
-        $query = "INSERT INTO users(username, user_email, user_password, user_role) ";
-        $query .= "VALUES('{$username}', '{$email}', '{$password}', 'subscriber')";
-        $register_user_query = mysqli_query($connection, $query);
-        if (!$register_user_query) {
-            die("Query Failed" . mysqli_error($connection));
-        }
-
-        $message = "Your Registration has been submitted";
-
-    } else {
-        $message = "Fields cannot be empty";
-    }
-} else {
-    $message = "";
 }
-
 
 ?>
 
@@ -67,11 +28,11 @@ if (isset($_POST['submit'])) {
                 <div class="col-xs-6 col-xs-offset-3">
                     <div class="form-wrap">
                         <h1>Contact</h1>
-                        <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
+                        <form role="form" action="" method="post" id="login-form" autocomplete="off">
                             <h6 class="text-center"><?php if (!empty($message)) {
                                                         echo $message;
                                                     } ?></h6>
-                            
+
                             <div class="form-group">
                                 <label for="email" class="sr-only">Email</label>
                                 <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
