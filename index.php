9245cd58fd76710c63a79346ef76564d5fc673e8
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-15 09:50:02 +0530

    UI Refactor

--- index.php
+++ index.php
@@ -9,15 +9,10 @@ include "includes/navigation.php";
 
 <!-- Page Content -->
 <div class="container">
-
     <div class="row">
-
         <!-- Blog Entries Column -->
         <div class="col-md-8">
-
             <?php
-
-            
             if (isset($_GET['page'])) {
                 $page = $_GET['page'];
             } else {
@@ -29,12 +24,9 @@ include "includes/navigation.php";
                 $page_1 = ($page * 5) - 5;
             }
 
-
-
             $post_query_count = "SELECT * FROM posts";
             $find_count = mysqli_query($connection, $post_query_count);
             $count = mysqli_num_rows($find_count);
-
             $count = ceil($count / 5);
 
             $query = "SELECT * FROM posts WHERE post_status = 'published' ORDER BY post_id DESC LIMIT $page_1, 5";
@@ -55,72 +47,52 @@ include "includes/navigation.php";
                 if ($post_status !== 'published') {
                     continue;
                 }
-
-
             ?>
-
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
-
-                <!-- First Blog Post -->
-
-                <h2>
-                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="author_posts.php?author=<?php echo $post_author; ?>&p_id=<?php echo $post_id; ?>"><?php echo $post_author; ?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
-                <hr>
-                <a href="post.php?p_id=<?php echo $post_id; ?>">
-                    <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                </a>
-
-                <hr>
-                <p><?php echo $post_content; ?></p>
-                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                </a>
-
-                <hr>
-
-
-
+                <article class="blog-post">
+                    <div class="post-content">
+                        <h2>
+                            <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
+                        </h2>
+                        <p class="lead">
+                            by <a href="author_posts.php?author=<?php echo $post_author; ?>&p_id=<?php echo $post_id; ?>"><?php echo $post_author; ?></a>
+                        </p>
+                        <div class="post-meta">
+                            <span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?>
+                        </div>
+                        <a href="post.php?p_id=<?php echo $post_id; ?>">
+                            <img src="images/<?php echo $post_image; ?>" alt="<?php echo $post_title; ?>">
+                        </a>
+                        <p class="mt-3"><?php echo $post_content; ?></p>
+                        <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">
+                            Read More <span class="glyphicon glyphicon-chevron-right"></span>
+                        </a>
+                    </div>
+                </article>
             <?php
             }
             ?>
-
-
         </div>
-
 
         <!-- Blog Sidebar Widgets Column -->
         <?php
         include "includes/sidebar.php";
         ?>
-
-
-
-
     </div>
-    <!-- /.row -->
 
-    <hr>
-
-    <ul class="pager">
-        <?php
-        for ($i = 1; $i <= $count; $i++) {
-            if ($i == $page) {
-                echo "<li><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
-            } else {
-                echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+    <!-- Pagination -->
+    <nav aria-label="Page navigation">
+        <ul class="pager">
+            <?php
+            for ($i = 1; $i <= $count; $i++) {
+                if ($i == $page) {
+                    echo "<li><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
+                } else {
+                    echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+                }
             }
-            
-        }
-        ?>
-    </ul>
+            ?>
+        </ul>
+    </nav>
 
     <!-- Footer -->
     <?php
